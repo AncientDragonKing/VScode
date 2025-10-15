a0e915db8058a2af8e93aa1b700f66b5b992d12c
Nereus Chen <nereuschen0122@gmail.com>
2025-10-15 21:19:34 +0800

    new file

---
+++ learn_coding/JavaScript/class1_basics_of_js/type.js
@@ -0,0 +1,36 @@
+
+// 變數
+// 這兩個程式都是拿來宣告變數的
+// 在 JavaScript 中，宣告變數有三種方式：var、let 和 const
+// 後兩種是最常見且最常用的
+
+let name = 'Jack';
+const age = 30;
+
+// 在所有程式後面加上分號; 是好的寫法
+// ; 是一段程式碼的結尾
+// 這樣可以避免一些潛在的錯誤
+// 也可以讓程式碼變清晰
+
+
+// 輸出
+console.log("Hello, JavaScript!");
+
+// 基本型別
+
+// JavaScript 有六種基本型別：
+// Number、String、Boolean、Null、Undefined 和 Symbol
+
+// 這些型別可以用來表示不同的值
+
+// Number：數字型別，可以是整數或浮點數
+// String：字串型別，用來表示文字
+// Boolean：布林型別，只有兩個值：true 和 false
+// Null：表示沒有值的特殊型別 = python 的 None
+// Undefined：表示未定義的值 -> 目前沒有值
+// Symbol：一種新的型別，用來表示唯一的值
+
+console.log('Null和Undefined的差別:');
+console.log('nullg4故意留空, undefined類似找不到值:');
+
+
